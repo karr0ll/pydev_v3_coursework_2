84fbeb82fab48910f6de05e90637cabda3c036df
Kirill Tuchkin <volkirtu@gmail.com>
2023-05-24 14:24:34 +0300

    изменен базовый класс интерфейса работы с файлами

--- base_classes/file_interface_base_class.py
+++ base_classes/file_interface_base_class.py
@@ -2,15 +2,34 @@ from abc import ABC, abstractmethod
 
 
 class FileInterfaceBaseClass(ABC):
+    """
+    Базовый класс для работы с файлами вакансий
+    """
 
     @abstractmethod
     def save_vacancies_to_file(self):
+        """
+        Абстрактный метод сохранения информации в файл
+        Returns: None
+
+        """
         pass
 
     @abstractmethod
-    def load_vacancies_from_file(self, api_service):
+    def load_vacancies_from_file(self):
+        """
+        Абстрактный метод загрузки информации из файла
+        Returns: None
+
+        """
         pass
 
     @abstractmethod
-    def delete_vacancy_from_file(self, api_service):
+    def delete_vacancy_from_file(self, vacancy_id):
+        """
+        Абстрактный метод удаления информации из файла по id вакансии
+        Params: id вакансии
+        Returns: None
+
+        """
         pass
