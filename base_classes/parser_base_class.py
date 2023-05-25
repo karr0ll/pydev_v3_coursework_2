f07b5da6b5e79d8195c6a9901d2958d64de39c3f
Kirill Tuchkin <volkirtu@gmail.com>
2023-05-25 11:33:11 +0300

    код проверен flake8

--- base_classes/parser_base_class.py
+++ base_classes/parser_base_class.py
@@ -4,7 +4,12 @@ from abc import ABC, abstractmethod
 class BaseApiParser(ABC):
 
     @abstractmethod
-    def get_vacancies_data(self, search_keyword, experience=None, schedule=None, area=None) -> dict:
+    def get_vacancies_data(self,
+                           search_keyword,
+                           experience=None,
+                           schedule=None,
+                           area=None
+                           ) -> dict:
         pass
 
 
