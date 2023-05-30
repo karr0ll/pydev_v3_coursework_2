72bfd4846f8b0816f8b60d3ac8e2a1d663659dcf
Kirill Tuchkin <volkirtu@gmail.com>
2023-05-30 15:39:52 +0300

    изменена логика класса

--- vacancies/headhunter_vacancy.py
+++ vacancies/headhunter_vacancy.py
@@ -6,10 +6,6 @@ class HeadHunterVacancy:
     Класс вакансий с HeadHunter, инициализируется любым из именованных аргументов
     """
     all = []
-    text: str = None
-    experience: str = None
-    area: str = None
-    salary: int = None
 
     def __init__(self, text=None, experience=None, area=None, salary=None) -> None:
         """
@@ -24,28 +20,54 @@ class HeadHunterVacancy:
         :param salary: зарплата
         :type salary: int
         """
-        self.text = text
-        self.experience = experience
-        self.area = area
-        self.salary = salary
+        if not isinstance(text, str):
+            raise TypeError("Ключевое слово для поиска должно быть строкой")
+        else:
+            self.text = text
+
+        if experience is not None:
+            if not isinstance(experience, str):
+                raise TypeError("Опыт работы для поиска долже быть строкой")
+        else:
+            self.experience = experience
+
+        if not isinstance(area, str):
+            raise TypeError("ID города для поиска долже быть строкой")
+        else:
+            self.area = area
+
+        if not isinstance(salary, int):
+            raise TypeError("Зарплата для поиска должны быть целым числом")
+        else:
+            self.salary = salary
 
         vacancy_data = self.__get_vacancy_data()
         for item in vacancy_data:
-            try:
-                self.employer_name: str = item["employer"]["name"]
-                self.vacancy_name: str = item["name"]
-                self.salary_from: int = item["salary"]["from"]
-                self.salary_to: int = item["salary"]["to"]
-                self.requirement: str = item["snippet"]["requirement"]
-                self.description: str = item["snippet"]["responsibility"]
-                self.employment: str = item["employment"]["name"]
-                self.url: str = item["alternate_url"]
-                self.all.append(self)
+            self.employer_name: str = item["employer"]["name"]
+            self.area: str = item["area"]["name"]
+            self.vacancy_name: str = item["name"]
+            self.salary_from: int = item["salary"]["from"]
+            self.salary_to: int = item["salary"]["to"]
+            self.requirement: str = item["snippet"]["requirement"]
+            self.experience: str = item["experience"]["name"]
+            self.description: str = item["snippet"]["responsibility"]
+            self.employment: str = item["employment"]["name"]
+            self.url: str = item["alternate_url"]
 
-            except TypeError:
-                for key, value in self.__dict__.items():
-                    if value is None:
-                        self.__dict__[key] = "не указано"
+            self.all.append(self)
+
+    # def __repr__(self) -> str:
+    #     """
+    #     Возвращает строковое представление экземпляра класса
+    #     :return: строка с представлением экземпляра класса
+    #     :rtype: str
+    #     """
+    #     return f"Компания: {self.employer_name}\nПозиция: {self.vacancy_name}, г.{self.area}\n" \
+    #            f"Зарпалата: от {self.salary_from} до {self.salary_to} руб\n" \
+    #            f"Требования к соискателю:\n{self.requirement}\n" \
+    #            f"Опыт работы: {self.experience}\n" \
+    #            f"Обязанности: {self.description}\n" \
+    #            f"Тип занятости: {self.employment}\nСсылка на вакансию: {self.url}\n"
 
     def __str__(self) -> str:
         """
@@ -54,24 +76,14 @@ class HeadHunterVacancy:
         :rtype: str
         """
         return f"Компания: {self.employer_name}\nПозиция: {self.vacancy_name}, г.{self.area}\n" \
-               f"Зарпалата: {self.salary_from} - {self.salary_to} руб\n" \
+               f"Зарпалата: от {self.salary_from} до {self.salary_to} руб\n" \
                f"Требования к соискателю:\n{self.requirement}\n" \
                f"Опыт работы: {self.experience}\n" \
                f"Обязанности: {self.description}\n" \
-               f"Тип занятости: {self.employment}\nСсылка на вакансию: {self.url} "
+               f"Тип занятости: {self.employment}\nСсылка на вакансию: {self.url}\n"
 
-    def __repr__(self) -> str:
-        """
-        Возвращает строковое представление экземпляра класса
-        :return: строка с представлением экземпляра класса
-        :rtype: str
-        """
-        return f"Компания: {self.employer_name}\nПозиция: {self.vacancy_name}, г.{self.area}\n" \
-               f"Зарпалата: {self.salary_from} - {self.salary_to} руб.\n" \
-               f"Требования к соискателю:\n{self.requirement}\n" \
-               f"Опыт работы: {self.experience}\n" \
-               f"Обязанности: {self.description}\n" \
-               f"Тип занятости: {self.employment}\nСсылка на вакансию: {self.url} "
+    def __ge__(self, other):
+        self.salary_from > other.salary_from
 
     def __get_vacancy_data(self) -> list[dict]:
         """
@@ -91,3 +103,7 @@ class HeadHunterVacancy:
 
 
 
+
+data = HeadHunterVacancy(text="python", salary=100_000, area="1")
+
+print(data.all)
