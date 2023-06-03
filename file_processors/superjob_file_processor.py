fa062484a3c84c7d282a3e30d72c783eede224c3
Kirill Tuchkin <volkirtu@gmail.com>
2023-06-03 20:59:38 +0300

    финальная реализация функционала проекта

--- file_processors/superjob_file_processor.py
+++ file_processors/superjob_file_processor.py
@@ -1,90 +1,26 @@
 import json
 
-from base_classes.file_interface_base_class import FileInterfaceABCClass
+from file_processors.file_processor_base_class import FileProcessorBaseClass
+from file_processors.json_mapper import JSONMapper
 from parsers.superjob_parser import SuperJobParser
 
 
-class SuperJobFileProcessor(FileInterfaceABCClass):
+class SuperJobFileProcessor(FileProcessorBaseClass):
     """
     Класс для работы с файлами вакансий, полученных через API
     """
-
-    def save_vacancies_to_file(self, *args, **kwargs) -> None:
-        with open("vacancies_sj.json", "w", encoding="UTF-8") as file:
-            data: list = self.map_json_data(**kwargs)
-            json.dump(data, file, indent=2, ensure_ascii=False)
-
-    @staticmethod
-    def map_json_data(**kwargs) -> list[dict]:
+    def save_vacancies_to_file(self, *args: str, **kwargs: str) -> None:
         """
-        Маппит полученный от API список словарей с данными вакансий.
-        :param kwargs: опциональные параметры запроса к API: keyword, payment_from, town, experience
-        :type kwargs: keyword: str, payment_from: int, town: int, experience: str
-        :return:  None
-        :rtype: None
-        """
-        data: dict = SuperJobParser().get_vacancies_data(**kwargs)
-        formatted_dicts_list = []
-        for item in data["objects"]:
-            formatted_dicts_list.append(
-                {
-                    "service": "SuperJob",
-                    "id": item["id"],
-                    "name": item["profession"],
-                    "city": {
-                        "id": item["town"]["id"],
-                        "name": item["town"]["title"]
-                    },
-                    "salary_from": item["payment_from"],
-                    "salary_to": item["payment_to"],
-                    "url": item["link"],
-                    "employer": item["client"]["title"],
-                    "requirement": None,
-                    "responsibility": item["candidat"],
-                    "experience": {
-                        "id": item["experience"]["id"],
-                        "name": item["experience"]["title"]
-                    },
-                    "employment": {
-                        "id": item["type_of_work"]["id"],
-                        "name": item["type_of_work"]["title"]
-                    }
-                }
-            )
-        return formatted_dicts_list
-
-    def load_vacancies_from_file(self) -> list[dict]:
-        """
-        Загружает словарь с данными вакансий из json-файла
-        :return: словарь с данными вакансий
-        :rtype: dict
-        """
-        with open("vacancies_sj.json", "r", encoding="UTF-8") as file:
-            vacancies_data: list[dict] = json.load(file)
-            return vacancies_data
-
-    def delete_vacancy_from_file(self, *args, **kwargs) -> None:
-        """
-        Удаляет вакансию из файла по id вакансии
-        :param vacancy_id: id вакансии
-        :type vacancy_id: str
+        Переопределяет метод базового класса. Сохраняет данные вакансии в файл
+        :param args: имя файла для сохранения данных
+        :type args: str
+        :param kwargs: имя файла для сохранения данных
+        :type kwargs: str
         :return: None
         :rtype: None
         """
-        vacancy_id = kwargs["vacancy_id"]
-        data: list[dict] = self.load_vacancies_from_file()
-
-        with open("vacancies_sj.json", "w+",
-                  encoding="UTF-8"
-                  ) as file:
-
-            for item in data:
-                if item["id"] == str(vacancy_id):
-                    item_index = data.index(item)
-                    del data[item_index]
-                    print(f"Вакансия id={vacancy_id} удалена")
-                    break
-                else:
-                    print(f"Вакансия id={vacancy_id} не найдена")
-                    break
-            json.dump(data, file, indent=2, ensure_ascii=False)
+        filename = kwargs["filename"]
+        data: dict = SuperJobParser().get_vacancies_data(**kwargs)
+        mapped_data: list[dict] = JSONMapper().map_sj_json_data(data)
+        with open(filename, "w", encoding="UTF-8") as file:
+            json.dump(mapped_data, file, indent=2, ensure_ascii=False)
